af2ba22cd96acc00f162c4c5f7dc4db95d31f805
dnaz <zibranaby069@gmail.com>
2025-09-03 13:50:05 +0700

    fix(user/update/passwords) fixing some errors that make /user/update/passowrds gone 500 fix refactor feat a bit

--- app/Core/Database.py
+++ app/Core/Database.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 from app.Core.Env import setting
 
-engine = create_engine(f'mysql+mysqlconnector://{setting.DB_USERNAME}:{setting.DB_PASSWORD}@{setting.DB_HOST}:{setting.DB_PORT}/{setting.DB_DATABASE}')
+engine = create_engine(f'mysql+mysqlconnector://{setting.DB_USERNAME}:{setting.DB_PASSWORD}@{setting.DB_HOST}:{setting.DB_PORT}/{setting.DB_DATABASE}',
+ pool_pre_ping=True,   #  cek koneksi sebelum dipakai
+    pool_recycle=3600,    #  recycle tiap 1 jam
+#antisipasi koneksi sebelum di pakek 
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
